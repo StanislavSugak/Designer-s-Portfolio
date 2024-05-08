f5b0f3981ef86fb75e7786779b4a91b48d41135f
Stanislav <stanislav71492584@gmail.com>
2024-05-08 10:54:56 +0300

    fix: add json file for en-ru and change js code

--- js/about/interpreter.js
+++ js/about/interpreter.js
@@ -1,169 +1,18 @@
-const langArr = {
-   "about":{
-        "ru": "О себе",
-        "en": "About",
-   },
-   "services":{
-        "ru": "Сервис",
-        "en": "Services",
-    },
-    "works":{
-        "ru": "Работа",
-        "en": "Works",
-    },
-    "blog":{
-        "ru": "Блог",
-        "en": "Blog",
-    },
-    "about1":{
-        "ru": "О себе",
-        "en": "About",
-   },
-   "services1":{
-        "ru": "Сервис",
-        "en": "Services",
-    },
-    "works1":{
-        "ru": "Работа",
-        "en": "Works",
-    },
-    "blog1":{
-        "ru": "Блог",
-        "en": "Blog",
-    },
-    "language":{
-        "ru": "Язык",
-        "en": "Language",
-    },
-    "russian":{
-        "ru": "Русский",
-        "en": "Russia",
-    },
-    "english":{
-        "ru": "Английский",
-        "en": "English",
-    },
-    "authorization":{
-        "ru": "Авторизация",
-        "en": "Authorization",
-    },
-    "sign-up":{
-        "ru": "Вход",
-        "en": "Sign up",
-    },
-    "registration":{
-        "ru": "Регистрация",
-        "en": "Registration",
-    },
-    "theme":{
-        "ru": "Тема",
-        "en": "Theme",
-    },
-    "hello":{
-        "ru": "Привет,",
-        "en": "Hello,",
-    },
-    "adam":{
-        "ru": "Адам Закоб",
-        "en": "Adam Zakob",
-    },
-    "freelance":{
-        "ru": "внештатный UX-дизайнер",
-        "en": "a freelance UX Designer",
-    },
-    "talk":{
-        "ru": "Поговорим?",
-        "en": "Let&#39s Talk",
-    },
-    "happiness":{
-        "ru": "Блаженство",
-        "en": "Happiness",
-    },
-    "take-a-look":{
-        "ru": "Загляните на сайт, а затем внизу вы сможете пообщаться со мной поближе",
-        "en": "Take a look at the site, and then at the bottom you can chat with me closer",
-    },
-    "portfolio":{
-        "ru": "Портфолио",
-        "en": "Portfolio",
-    },
-    "all-creative":{
-        "ru": "Все лучшие проекты",
-        "en": "All Creative Projects",
-    },
-    "explore":{
-        "ru": "Больше",
-        "en": "Explore More",
-    },
-    "oops":{
-        "ru": "Упсс",
-        "en": "Oops",
-    },
-    "there-are-no-more":{
-        "ru": "На этой странице пока больше нет работ, но вы можете найти больше в моих социальных сетях",
-        "en": "There are no more works on this page yet, but you can find more on my social networks",
-    },
-    "my-instagram":{
-        "ru": "Мой инстаграм",
-        "en": "My instagram",
-    },
-    "clients":{
-        "ru": "Клиенты",
-        "en": "Clients",
-    },
-    "big-deal":{
-        "ru": "Проекты вместе с",
-        "en": "Big Deal With",
-    },
-    "testimonials":{
-        "ru": "Рекомендации",
-        "en": "Testimonials",
-    },
-    "what-people":{
-        "ru": "Что говорят люди",
-        "en": "What People Says",
-    },
-    "quote-testimonials":{
-        "ru": "Цитируйте отзывы, демонстрирующие поддержку вашего продукта или функции пользователем, знающие это. Это будет лучше некоторых методов рекламы, поскольку потребители будут доверять вам.",
-        "en": "Quote testimonials demonstrate support for your product or feature from a user who has experience with it. This can be significantly more effective than traditional advertising methods as most consumers will trust.",
-    },
-    "address":{
-        "ru": "Адрес",
-        "en": "Address",
-    },
-    "where-you":{
-        "ru": "Где найдете вы меня",
-        "en": "Where you'll find me",
-    },
-    "contact-me":{
-        "ru": "Свяжитесь со мной",
-        "en": "Contact Me",
-    },
-    "let-me":{
-        "ru": "Дайте мне знать, если захотите узнать о совместном проекте. Я готов работать фрилансером.",
-        "en": "Let me know if you want to talk about a potential collaboration. I'm available for freelance work.",
-    },
-    "your-name":{
-        "ru": "Как тебя зовут?",
-        "en": "What's your name?",
-    },
-    "your-email":{
-        "ru": "Ваш электронный адрес?",
-        "en": "Your email?",
-    },
-    "your-project":{
-        "ru": "Расскажите мне о проекте",
-        "en": "Tell me about your project",
-    },
-    "get-quote":{
-        "ru": "Мое предложение",
-        "en": "Get a Quote",
-    },
-    "friends":{
-        "ru": "Давай дружить",
-        "en": "Let's be Friends",
-    },
-}
+var langArr;
+
+fetch('../js/about/en-ru.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Ой, ошибка в fetch: ' + response.statusText);
+    }
+    return response.json();
+  })
+.then(jsonData => {
+    langArr = jsonData; 
+    loadLanguage();
+})
+.catch(error => console.error('Ошибка при исполнении запроса: ', error));
+
 
 window.addEventListener('DOMContentLoaded', function() {
     const savedLanguage = localStorage.getItem('selectedLanguage');
@@ -175,7 +24,7 @@ window.addEventListener('DOMContentLoaded', function() {
         location.reload();
       }
     }
-  });
+});
 
 const myLang = ['ru', 'en'];
 
@@ -211,5 +60,3 @@ function loadLanguage() {
     document.querySelector('.lng-' + key).innerHTML = langArr[key][hash];
   }
 }
-
-loadLanguage();
