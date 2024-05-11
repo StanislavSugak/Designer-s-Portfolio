0b3a38eadc759bff80fb72b316f83de9623cfdff
Stanislav <stanislav71492584@gmail.com>
2024-05-11 18:27:20 +0300

    feat: add more input field and js code

---
+++ js/signup-in/checknickname.js
@@ -0,0 +1,39 @@
+var formNickname = document.getElementById('nickname');
+
+var inputElementNickname = formNickname.querySelector('input');
+
+var labelElementNickname = formNickname.querySelector('.input__error');
+
+var button_nickname = formNickname.querySelector('button');
+
+inputElementNickname.addEventListener('input', removeErrorClass);
+button_nickname.addEventListener('click', autonickname);
+
+function removeErrorClass() {
+    var inputValue = inputElementNickname.value;
+    console.log( inputValue);
+
+    if (inputValue.length > 20 || inputValue.length < 2 || inputValue.trim() === '' || inputValue.indexOf(' ') !== -1) {
+        labelElementNickname.classList.add('active');
+    }else{
+        labelElementNickname.classList.remove('active');
+    }
+}
+
+function autonickname(event) {
+    event.preventDefault();
+
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var nickname = '';
+  
+    for (var i = 0; i < 16; i++) {
+      var randomIndex = Math.floor(Math.random() * characters.length);
+      nickname += characters.charAt(randomIndex);
+    }
+  
+    inputElementNickname.value = ' ';
+    setTimeout(() => {
+        inputElementNickname.value = nickname;
+    }, 250);
+}
+  
