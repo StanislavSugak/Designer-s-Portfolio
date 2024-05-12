f79352d4a62e6e7be2199446d1cd3df10049541b
Stanislav <stanislav71492584@gmail.com>
2024-05-12 14:15:26 +0300

    feat: add registration in localStorage

--- js/signup-in/signup.js
+++ js/signup-in/signup.js
@@ -1,18 +1,23 @@
 var signupCont = document.querySelector('.lower-btn');
 
+for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    const value = localStorage.getItem(key);
+    console.log(`${key}: ${value}`);
+}
+
 var signBtn = signupCont.querySelector('p');
 
 signBtn.addEventListener('click', readdata);
 
 var field = ['name', 'surname', 'patronymic','phone','email','birth','nickname', 'password', 'passwordRepead','agree'];
 var check = true;
+var user;
 
 function readdata(){
     if(checkdata()){
-        console.log('if' + check);
-    }
-    else{
-        console.log('else' + check)
+        writedata();
+        location.reload(); 
     }
 }
 
@@ -22,7 +27,6 @@ function checkdata(){
     var contant = document.querySelector('.sign-up-content');
     var act = document.querySelectorAll('.active');
 
-    console.log(act);
     if(act.length === 0){
         for(let i = 0; i  < field.length; i++){
             var form = document.getElementById(field[i]);
@@ -39,9 +43,9 @@ function checkdata(){
                 }else{
                     if(i == 5){
                         var input = form.querySelectorAll('input');
-                        
+
                         for(let i = 0; i < input.length; i++){
-                            if(input.value == ''){
+                            if(input[i].value === ''){
                                 input_error.classList.add('active');
                                 check = false;
                             }
@@ -65,3 +69,64 @@ function checkdata(){
 
     return check;
 }
+
+async function writedata(){
+    const storedData = localStorage.getItem('user');
+    user = JSON.parse(storedData);
+
+    var newUser = setnewUser();
+
+    user.push(newUser);
+
+    RewriteUser(user);
+}
+
+async function getUser(){
+    try {
+        const response = await fetch('../js/signup-in/users.json');
+        if (!response.ok) {
+          throw new Error('Ой, ошибка в fetch: ' + response.statusText);
+        }
+        const jsonData = await response.json();
+        return jsonData;
+    } catch (error) {
+        console.error('Ошибка при исполнении запроса: ', error);
+        return null;
+    }
+}
+
+function setnewUser(){
+    var data = document.querySelector('.sign-up-content');
+    var data_input = data.querySelectorAll('input');
+
+    var newUser = {
+        name: data_input[0].value,
+        surname : data_input[1].value,
+        patronymic: data_input[2].value,
+        phone: data_input[3].value,
+        email: data_input[4].value,
+        birth: data_input[5].value + '.' + data_input[6].value + '.' + data_input[7].value,
+        nickname: data_input[8].value,
+        password: data_input[9].value,
+        role: 'user'
+    }
+
+    return newUser;
+}
+
+function RewriteUser(user){
+    const jsonString = JSON.stringify(user);
+    localStorage.setItem('user', jsonString);
+}
+
+window.addEventListener('DOMContentLoaded', async function() {
+    const storedData = localStorage.getItem('user');
+    user = JSON.parse(storedData);
+
+    if(storedData === null){
+        user = await getUser();
+        console.log(user);
+        const usersString = JSON.stringify(user);
+        localStorage.setItem('user', usersString);
+    }
+});
