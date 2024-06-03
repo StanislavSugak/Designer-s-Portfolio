4f39660357ebc076d5b339b36ebe6cc74590519b
Stanislav <stanislav71492584@gmail.com>
2024-06-03 21:34:35 +0300

    fix: change link on js

--- js/common/dropdown.js
+++ js/common/dropdown.js
@@ -5,5 +5,5 @@ function dropdown() {
 const dropBtn = document.querySelector('.dropdown-toggle');
 
 dropBtn.addEventListener('dblclick', function(){
-    location.href = "../pages/about.html";
+    location.href = "../index.html";
 });
